1fb3daa7df7e4489ce833955a3f472b4e7eae3a7
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-06-23 15:42:36 -0500

    Changed sti to bonus and added sti function

--- group_01.js
+++ group_01.js
@@ -7,9 +7,19 @@ var employees = [atticus, jem, boo, scout];
 var employeesTotal = employees[];
 
 for(var i = 0; i < employees.length; i++){
-  employeesTotal[i] = sti(employees);
+  employeesTotal[i] = bonus(employees);
 }
 
-function sti(emp){
-  
+function sti(emp, percentage){
+  if(emp[3] <= 2){
+    percentage
+  }
+}
+function bonus(emp){
+  int temp = 0;
+  if(emp[2] === 4){
+    temp = 5;
+  }
+  sti(emp, temp);
+  return temp;
 }

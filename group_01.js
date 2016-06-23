a7e258dbac7d17fe0619a25f725efaf74203806f
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-06-23 16:04:11 -0500

    corrected mistakes in basic logic and other typos

--- group_01.js
+++ group_01.js
@@ -4,31 +4,43 @@ var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
-var employeesTotal = employees[];
+var employeesTotal = employees;
 
 for(var i = 0; i < employees.length; i++){
-  employeesTotal[i] = bonus(employees);
+  employeesTotal[i][1] = bonus(employees[i]);
+  console.log(employeesTotal[i]);
+
 }
 
-function sti(emp, percentage){
-  if(emp[3] <= 2){
+function sti(emp2, percentage){
+  if(emp2[3] <= 2){
     return percentage;
   }
-  if(emp[3] === 3){
+  if(emp2[3] === 3){
     return percentage += 4;
   }
-  if(emp[3] === 4){
+  if(emp2[3] === 4){
     return percentage += 6;
   }
-  if(emp[3] === 5){
+  if(emp2[3] === 5){
     return percentage += 10;
   }
+  return percentage;
 }
 function bonus(emp){
-  int temp = 0;
-  if(emp[2] === 4){
+  var temp = 0;
+  if(emp[1].length === 4){
     temp = 5;
   }
-  sti(emp, temp);
+  temp = sti(emp, temp);
+  if(emp[2] >= 65000){
+    temp -= 1;
+    if(temp < 0){
+      temp = 0;
+    }
+  }
+  if(temp > 13){
+    temp = 13;
+  }
   return temp;
 }
